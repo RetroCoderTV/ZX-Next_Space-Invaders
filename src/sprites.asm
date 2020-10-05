3c00e0eac6f2734b7c54b3b45bbdafaaf3073e86
RetroCoderTV <coderretro409@gmail.com>
2020-10-05 03:57:44 +0100

    Eureka#2

--- src/sprites.asm
+++ src/sprites.asm
@@ -1,3 +1,24 @@
+; Sprite Attribute 0
+;     X X X X X X X X
+
+; Sprite Attribute 1
+;     Y Y Y Y Y Y Y Y
+
+; Sprite Attribute 2
+;     P P P P XM YM R X8/PR
+
+; Sprite Attribute 3
+;     V E N5 N4 N3 N2 N1 N0 (NNNNN=Pattern ID)
+
+; Sprite Attribute 4
+; A. Extended Anchor Sprite
+;     H N6 T X X Y Y Y8
+; B. Relative Sprite, Composite Type
+;     0 1 N6 X X Y Y PO
+; C. Relative Sprite, Unified Type
+;     0 1 N6 0 0 0 0 PO
+
+
 SPRITE_COUNT equ 3
 
 Sprite0:
