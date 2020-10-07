248a6d4bb1c562e73b1f3a8c93ab45605e5e55d5
RetroCoderTV <coderretro409@gmail.com>
2020-10-07 14:13:25 +0100

    added MMU comment/info

--- src/main.asm
+++ src/main.asm
@@ -45,3 +45,37 @@ stack_top db 0
 	SAVENEX AUTO
     SAVENEX CLOSE 
 
+
+
+;MMU notes (sjasm)
+; MMU <first slot number> [<last slot number>|<single slot option>], <page number>[,<address>]
+
+;     Maps memory page(s) to slot(s), similar to SLOT + PAGE combination, but allows to set up whole range of consecutive slots (with consecutive memory pages). Or when only single slot is specified, extra option can be used to extend particular slot functionality. The slot behaviour will stay set in the current DEVICE until reset by another MMU specifying same slot (even as part of range, that will clear the option to "default").
+
+;     The optional third argument is address for ORG functionality.
+
+;     Single slot option (default state is: no error/warning and no wrap = nothing special):
+
+;     e = error on writing beyond last byte of slot
+;     w = warning on writing beyond last byte of slot
+;     n = wrap address back to start of slot, map next page
+
+;     Example 5.34. docs_examples/po_mmu.asm
+
+;         DEVICE ZXSPECTRUM128 : LABELSLIST "po_mmu.lbl"  ; to check label pages
+;         MMU 1 3, 5      ; maps slots 1, 2, 3 with pages 5, 6, 7
+;         ORG 0xBFFF
+;     label1_p6: scf      ; last byte of page 6 (in slot 2)
+;     label2_p7: scf      ; first byte of page 7 (in slot 3)
+
+;         MMU 3 e, 0      ; page 0 into slot 3, write beyond slot will cause error
+;         ORG 0xFFFF
+;         ld  a,1         ; error: Write outside of memory slot: 65536 (65536 = address outside)
+
+;         MMU 3 n, 1      ; page 1 into slot 3, make it wrap + map next page automatically
+;         ORG 0xFFFF      ; ! also the $ address was truncated by MMU from $10001 to $0001 !
+;     label3_p1: scf      ; last byte of page 1, then wrapping back to 0xC000 with page 2
+;     label4_p2: scf      ; first byte of page 2 at 0xC000
+
+
+
